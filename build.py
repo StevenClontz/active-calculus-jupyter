5d3698650a27fd941d8595a9df8ebe3e543fcbb5
Steven Clontz <steven.clontz@gmail.com>
2021-02-17 15:02:59 +0000

    fix indentation level, add build dir to gitignore

--- build.py
+++ build.py
@@ -4,25 +4,25 @@ from shutil import copytree
 import click
 
 def make_directory(path):
-  if not os.path.exists(path):
-    os.makedirs(path)
+    if not os.path.exists(path):
+        os.makedirs(path)
 
 @click.command()
 @click.option('--previews/--no-previews', default=True, help='Include preview activities for each section.')
 def build(previews):
-  include_previews = "'yes'" if previews else "'no'"
-  book = etree.parse("apc/src/index.xml")
-  xsl = etree.parse("ac-to-jupyter.xml")
-  transform = etree.XSLT(xsl)
-  book.xinclude()
-  make_directory("activities")
-  for cindex, chapter in enumerate(book.xpath("//chapter"), start=1):
-    for sindex, section in enumerate(chapter.xpath("section"), start=1):
-      section_path = "activities/section-"+str(cindex)+"-"+str(sindex)+"-"+section.xpath('./@xml:id')[0]+".ipynb"
-      section_code = "'"+str(cindex)+"."+str(sindex)+"'"
-      transform(section, section=section_code, includepreviews=include_previews).write_output(section_path)
-  if not os.path.exists("activities/images"):
-    copytree("apc/src/images","activities/images")
+    include_previews = "'yes'" if previews else "'no'"
+    book = etree.parse("apc/src/index.xml")
+    xsl = etree.parse("ac-to-jupyter.xml")
+    transform = etree.XSLT(xsl)
+    book.xinclude()
+    make_directory("activities")
+    for cindex, chapter in enumerate(book.xpath("//chapter"), start=1):
+        for sindex, section in enumerate(chapter.xpath("section"), start=1):
+            section_path = "activities/section-"+str(cindex)+"-"+str(sindex)+"-"+section.xpath('./@xml:id')[0]+".ipynb"
+            section_code = "'"+str(cindex)+"."+str(sindex)+"'"
+            transform(section, section=section_code, includepreviews=include_previews).write_output(section_path)
+    if not os.path.exists("activities/images"):
+        copytree("apc/src/images","activities/images")
 
 if __name__ == '__main__':
-  build()
+    build()
